532487407f82e9d9084952d604160d314d9d18db
Harry-Gupenec <Spektar001@mail.ru>
2022-10-12 17:07:23 +0300

    feat: complete task carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,28 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+	const MODERN_ACTIVITY = 15;
+	const HALF_LIFE_PERIOD = 5730;
+	let end_activity = Number(sampleActivity);
+	let n = Math.log(MODERN_ACTIVITY / end_activity);
+	let k = 0.693 / HALF_LIFE_PERIOD;
+	let t = Math.ceil(n / k);
+
+if ( typeof sampleActivity !== "string" ||
+	 	isNaN(sampleActivity) ||
+	 	sampleActivity <= 0 ||
+	 	sampleActivity === null ||
+	 	sampleActivity === undefined ||
+	 	sampleActivity > MODERN_ACTIVITY ||
+	 	sampleActivity.length == 0 ) {
+		return false;
+	}
+
+	return t;
 }
 
 module.exports = {
