cf4ff23f52f22f83cc51d5fe227a8918647b1e4d
Harry-Gupenec <Spektar001@mail.ru>
2022-10-13 19:02:54 +0300

    feat: add completed task extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+	const separatorDefault = '+';
+  const additionSeparatorDefault = '|';
+  let result = String(str);
+  let repeatTimes = options.repeatTimes;
+  let separator = "";
+  let addition = "";
+  let additionString = "";
+  let additionSeparator = "";
+
+  additionString += (options.addition !== undefined) ? options.addition : "";
+  addition += additionString;
+  additionSeparator = options.additionSeparator || additionSeparatorDefault;
+  for(let i = 1; i < options.additionRepeatTimes; i++) {
+    addition += additionSeparator + additionString;
+  }
+  separator = options.separator || separatorDefault;
+  result += addition;
+  for(let i = 1; i < repeatTimes; i++) {
+    result += separator + str + addition;
+  }
+  return result;
 }
 
 module.exports = {
