4bd6497108acc6b2799f268b23f8b6630d18c0a9
Harry-Gupenec <Spektar001@mail.ru>
2022-10-15 01:12:55 +0300

    feat: add comleted task vigener-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,72 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type = true) {
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    this.type = type;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (!message || !key || typeof message !== 'string' || typeof key !== 'string') throw new Error ("Incorrect arguments!");
+    let messageCopy = message.toUpperCase().split(' ').join('');
+    let keyCopy = key.toUpperCase();
+    let preResult = '';
+    let finalResult = '';
+    for(;messageCopy.length > keyCopy.length;) {
+			keyCopy+=keyCopy;
+		}
+    for (let i = 0; i < messageCopy.length; i++) {
+      let newAlphabet = [...this.alphabet];
+      let codeAlphabet = newAlphabet.concat(newAlphabet.splice(0, newAlphabet.indexOf(keyCopy[i]))).join('');
+      if (this.alphabet.includes(messageCopy[i])) {
+				preResult += codeAlphabet[this.alphabet.indexOf(messageCopy[i])];
+			}
+      else {
+				preResult += messageCopy[i];
+			}
+    }
+    let reverseMessage = preResult.split('').reverse();
+    for (let i = 0; i < message.length; i++) {
+      if (message[i] != ' ') finalResult += reverseMessage.pop();
+      else finalResult += ' ';
+    }
+
+    if (this.type) {
+      return finalResult;
+    } else {
+      return finalResult.split('').reverse().join('');
+    }
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key || typeof encryptedMessage !== 'string' || typeof key !== 'string') throw new Error ("Incorrect arguments!");
+    let encryptedMessageCopy = encryptedMessage.toUpperCase().split(' ').join('')
+    let keyCopy = key.toUpperCase();
+    let preResult = '';
+    let finalResult = '';
+    for(;encryptedMessageCopy.length > keyCopy.length;) {
+			keyCopy+=keyCopy;
+		}
+    for (let i = 0; i < encryptedMessageCopy.length; i++) {
+      let newAlphabet = [...this.alphabet];
+      let codeAlphabet = newAlphabet.concat(newAlphabet.splice(0, newAlphabet.indexOf(keyCopy[i]))).join('');
+      if (this.alphabet.includes(encryptedMessageCopy[i])) {
+				preResult += this.alphabet[codeAlphabet.indexOf(encryptedMessageCopy[i])];
+			}
+      else {
+				preResult += encryptedMessageCopy[i];
+			}
+    }
+    let reverseEncryptedMessage = preResult.split('').reverse();
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      if (encryptedMessage[i] != ' ') finalResult += reverseEncryptedMessage.pop();
+      else finalResult += ' ';
+    }
+
+    if (this.type) {
+      return finalResult;
+    } else {
+      return finalResult.split('').reverse().join('');
+    }
   }
 }
 
